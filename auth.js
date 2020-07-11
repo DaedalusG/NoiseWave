153c74a7d2e6ef637fbff3a2070d1d6780eeadbb
chris <cgcottier@gmail.com>
2020-07-11 12:41:12 -0500

    added jwt config to .env and example, wrote notes for how to apply auth

---
+++ auth.js
@@ -0,0 +1,24 @@
+// What needs to be authenticated?
+// -Users
+
+//What does authentication require?
+//-valid user credentials from signing up or signing in
+
+// What route to sign up?
+
+// What route to sign in?
+
+// When do we need to authenticate?
+// -when user adds a comment
+// -when user adds a like
+//- when user tries to do anything requiring authorization
+
+// What/when do we need to authorize?
+// - when user tries to edit song
+// -when user tries to delete a song
+// -when user tries to edit profile
+// -when user tries to delete profile
+// -user deletes a comment
+//-user deletes a like
+
+//once user created with bcrypt encryption
