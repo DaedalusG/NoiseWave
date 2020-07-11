80eeced1062323f748bc2fb695f17fcb44c66aaa
chris <cgcottier@gmail.com>
2020-07-11 14:36:31 -0500

    started auth functions and changed hashedPasswrod type to string binary in db

--- auth.js
+++ auth.js
@@ -5,8 +5,10 @@
 //-valid user credentials from signing up or signing in
 
 // What route to sign up?
+// - .post('/users')
 
 // What route to sign in?
+// - I think it should be .post('/users/signin')
 
 // When do we need to authenticate?
 // -when user adds a comment
@@ -21,4 +23,60 @@
 // -user deletes a comment
 //-user deletes a like
 
-//once user created with bcrypt encryption
+//once user created with bcrypt encryption...
+
+const jwt = require("jsonwebtoken");
+const { jwtConfig } = require("./config");
+const { secret, expiresIn } = jwtConfig;
+
+const { User } = require("./db/models");
+const bearerToken = require("express-bearer-token");
+
+const generateUserToken = (user) => {
+  //we pass in the user instance from db, this is ran when users are signing up
+
+  const userDataForToken = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  const token = jwt.sign({ data: userDataForToken }, secret, {
+    expiresIn: parseInt(expiresIn, 10),
+  });
+
+  return token;
+};
+
+const authenticateUser = (req, res, next) => {
+  const { token } = req;
+
+  if (!token) {
+    return res.set("WWW-authenticate");
+  }
+};
+
+// THIS IS WHAT THE SIGNUP POST ROUTE SHOULD LOOK LIKE IN THE ROUTE FOLDER
+//this route encrypts the password, creates the new user, creates a token for that user, and sends the token in the repons to user.
+router.post(
+  "/users",
+  validateUsername,
+  validateEmailAndPassword,
+  handleValidationErrors,
+  asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, email, hashedPassword });
+
+    const token = generateUserToken(user);
+    res.status(201);
+
+    //This part im not sure, we want to give the client the token created and redirect them??
+    res.token = token;
+    res.redirect("I DONT KNOW");
+  })
+);
+
+module.exports = {
+  generateUserToken,
+};
