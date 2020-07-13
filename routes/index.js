d3f0e48ca82e83bceb7809b49f5609a4cf6e25df
chris <cgcottier@gmail.com>
2020-07-13 17:20:21 -0500

    moved routes around a bit, changed where tokens are generated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-const express = require('express');
+const express = require("express");
+const { asyncHandler, handleValidationErrors } = require("../utils");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
-module.exports = router
+router.post(
+  "/login",
+  handleValidationErrors,
+  asyncHandler(async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        username,
+        //or username, our choice, as long as unique
+      },
+    });
+    const validPassword = bcrypt.compareSync(
+      password,
+      user.hashedPassword.toString()
+    );
+
+    //This check should be in error validator.
+    //if credentials invalid, it errors
+    // if (!user || !validPassword) {
+    //   const err = new Error("Login failed");
+    //   err.status = 401;
+    //   err.title = "Login failed";
+    //   err.errors = ["The provided credentials were invalid."];
+    //   return next(err);
+    // }
+    const token = generateUserToken(user);
+    localStorage.setItem("NOISEWAVE_ACCESS_TOKEN", token);
+  })
+);
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,83 @@
-const { User } = require('../db/models');
-const { apiPort } = require('../config')
-const { asyncHandler, handleValidationErrors } = require('../utils');
+const { User } = require("../db/models");
+const { apiPort } = require("../config");
+const { asyncHandler, handleValidationErrors } = require("../utils");
 
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const cookieParser = require('cookie-parser');
-const csrf = require('csurf');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 
 const router = express.Router();
 
 router.use(cookieParser());
 
-const csrfProtection = csrf({cookie: true});
+const csrfProtection = csrf({ cookie: true });
 
-const getUser = async userId => {
-    const userData = await fetch(`http://localhost:${apiPort}/users/${userId}`);
-    return await userData.json();
-}
+const getUser = async (userId) => {
+  const userData = await fetch(`http://localhost:${apiPort}/users/${userId}`);
+  return await userData.json();
+};
 
 // User sign up form action
-router.post('/', handleValidationErrors, asyncHandler(async (req, res) => {
+router.post(
+  "/",
+  handleValidationErrors,
+  asyncHandler(async (req, res) => {
     // TODO save uploaded pictures to s3
     const { username, password, email } = req.body;
     const hashedPassword = await bcrypt.hash(password, 8);
-    await User.create({ username, hashedPassword, email });
+    const user = await User.create({
+      username,
+      hashedPassword,
+      email,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    //Give User token
+    const token = generateUserToken(user);
+    localStorage.setItem("NOISEWAVE_ACCESS_TOKEN", token);
+
     res.status(200);
-    res.redirect('/discover');
-}));
+    res.redirect("/discover");
+  })
+);
 
 // Renders a user edit form
-router.get('/:id(\\d+)/edit', csrfProtection, asyncHandler(async (req, res) => {
+router.get(
+  "/:id(\\d+)/edit",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
     const userId = req.params.id;
     const user = await getUser(userId);
-    res.render('user-edit', {user, csrfToken: req.csrfToken()});
-}));
+    res.render("user-edit", { user, csrfToken: req.csrfToken() });
+  })
+);
 
 // User profile edit form action
-router.put('/:id(\\d+)', handleValidationErrors, csrfProtection, asyncHandler(async (req, res) => {
+router.put(
+  "/:id(\\d+)",
+  handleValidationErrors,
+  csrfProtection,
+  asyncHandler(async (req, res) => {
     const userId = req.params.id;
     const user = await getUser(userId);
     const { username, password, email } = req.body;
-    const hashedPassword = password ? await bcrypt.hash(password, 8) : user.hashedPassword;
-    user.save({ username, hashedPassword, email })
+    const hashedPassword = password
+      ? await bcrypt.hash(password, 8)
+      : user.hashedPassword;
+    user.save({ username, hashedPassword, email });
     res.redirect(`/${username}`);
-}));
+  })
+);
 
-router.delete('/:id(\\d+)', asyncHandler(async (req, res) => {
+router.delete(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
     const user = await getUser(req.params.id);
     user.destroy();
     req.status(200);
-}))
+  })
+);
 
 module.exports = router;
