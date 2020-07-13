fb4156b71eff79c1a7763ccb083bb1dfea4a0d13
Ben Anderson <andersjbe@gmail.com>
2020-07-13 12:12:10 -0600

    Heroku

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/explore', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
