10196bbe9c698e6bc84f3aeeaa119f413c7691ae
chris <cgcottier@gmail.com>
2020-07-13 09:48:17 -0500

    added logout event listener

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,12 @@
 //listener on logout button. On click, if user is logged in, it logs them out and redirects them to splash page or explore.
+
+const logoutButton = document.querySelector(".log-out-button");
+
+document.addEventListener("DOMContentLoaded", () => {
+  logoutButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    localStorage.removeItem("NOISEWAVE_ACCESS_TOKEN");
+
+    window.location.href = "/explore";
+  });
+});
