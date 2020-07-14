8ef020a15aa96f81a122d201352667594b726b1f
warrgiff <warrenbruceg@gmail.com>
2020-07-14 15:22:48 -0700

    more prep to merge

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/users');
 const express = require("express");
 const morgan = require("morgan");
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -26,60 +27,30 @@ app.use('/songs', songRoutes);
 app.use('/users', userRoutes)
 
 //setting AWS credentials and initializing aws-sdk object instance
+// remember to import keys from config: const { awsKeys } = require('./config');
 AWS.config = new AWS.Config();
 AWS.config.accessKeyId = awsKeys.IAM_ACCESS_ID;
 AWS.config.secretAccessKey = awsKeys.IAM_SECRET;
 const S3 = new AWS.S3();
 
-//configuring the DiscStorage engine.
-const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
-
-//The uploadFile function
-function uploadFile(source, targetName, res) {
-  console.log('preparing to upload...');
-  fs.readFile(source, function (err, filedata) {
-    if (!err) {
-      const putParams = {
-        Bucket: 'noisewave',
-        Key: targetName,
-        Body: filedata
-      };
-      S3.putObject(putParams, (err, data) => {
-        console.log('we are live!!!');
-        //fs.unlink(source);// Deleting the file from uploads folder(Optional).Do Whatever you prefer.
-        console.log('Successfully uploaded the file');
-        res.send({ success: "true" });
-        // if (err) {
-        //   console.log('Could nor upload the file. Error :', err);
-        //   return res.send({ success: false });
-        // }
-        // else {
-        //   fs.unlink(source);// Deleting the file from uploads folder(Optional).Do Whatever you prefer.
-        //   console.log('Successfully uploaded the file');
-        //   return res.send({ success: true });
-        // }
-      });
+//setting up direct stream to s3 bucket using multer and multer-s3
+const upload = multer({
+  storage: multerS3({
+    s3: S3,
+    bucket: 'noisewave',
+    // metadata: function (req, file, cb) {
+    //   cb(null, { fieldName: file.fieldname });
+    // },
+    key: function (req, file, cb) {
+      cb(null, file.originalname)
     }
-    else {
-      console.log({ 'err': err });
-    }
-  });
-}
-
-//test path for uploading a file from localStorage to S3 noisewave bucket
-app.post('/post_file', upload.single('demo_file'), function (req, res) {
-  //Multer middleware adds file(in case of single file ) or files(multiple files) object to the request object.
-  //req.file is the demo_file
-  console.log(req.file.path);
-  uploadFile(req.file.path, req.file.filename, res);
+  })
 })
 
+//example route handler to post file
+app.post('/post_file', upload.single('to_s3'), function (req, res, next) {
+  res.send({ success: true })
+})
 
 // middleware to catch errors caused by unhandled requests
 app.use((req, res, next) => {
