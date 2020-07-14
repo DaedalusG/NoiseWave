929f6b054009953351187cfce482fb80e90b77a1
warrgiff <warrenbruceg@gmail.com>
2020-07-14 11:08:29 -0700

    working upload

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const multer = require('multer');
 const AWS = require('aws-sdk');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
+
+const { awsKeys } = require('./config');
 
 const app = express();
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,6 +24,62 @@ app.use(express.json());
 app.use('/', indexRoutes);
 app.use('/songs', songRoutes);
 app.use('/users', userRoutes)
+
+//setting AWS credentials and initializing aws-sdk object instance
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = awsKeys.IAM_ACCESS_ID;
+AWS.config.secretAccessKey = awsKeys.IAM_SECRET;
+const S3 = new AWS.S3();
+
+//configuring the DiscStorage engine.
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+//The uploadFile function
+function uploadFile(source, targetName, res) {
+  console.log('preparing to upload...');
+  fs.readFile(source, function (err, filedata) {
+    if (!err) {
+      const putParams = {
+        Bucket: 'noisewave',
+        Key: targetName,
+        Body: filedata
+      };
+      S3.putObject(putParams, (err, data) => {
+        console.log('we are live!!!');
+        //fs.unlink(source);// Deleting the file from uploads folder(Optional).Do Whatever you prefer.
+        console.log('Successfully uploaded the file');
+        res.send({ success: "true" });
+        // if (err) {
+        //   console.log('Could nor upload the file. Error :', err);
+        //   return res.send({ success: false });
+        // }
+        // else {
+        //   fs.unlink(source);// Deleting the file from uploads folder(Optional).Do Whatever you prefer.
+        //   console.log('Successfully uploaded the file');
+        //   return res.send({ success: true });
+        // }
+      });
+    }
+    else {
+      console.log({ 'err': err });
+    }
+  });
+}
+
+//test path for uploading a file from localStorage to S3 noisewave bucket
+app.post('/post_file', upload.single('demo_file'), function (req, res) {
+  //Multer middleware adds file(in case of single file ) or files(multiple files) object to the request object.
+  //req.file is the demo_file
+  console.log(req.file.path);
+  uploadFile(req.file.path, req.file.filename, res);
+})
+
 
 // middleware to catch errors caused by unhandled requests
 app.use((req, res, next) => {
