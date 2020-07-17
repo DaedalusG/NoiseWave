82e8df5dafb586119a730c5cb0c91c33923c734e
Ben Anderson <andersjbe@gmail.com>
2020-07-17 10:27:55 -0600

    Created page for testing audio player

--- routes/index.js
+++ routes/index.js
@@ -109,7 +109,8 @@ router.get(
     if (
       username === "login" ||
       username === "search" ||
-      username === "explore"
+      username === "explore" ||
+      username === 'audio-test'
     ) {
       next();
       return;
