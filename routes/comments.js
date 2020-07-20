26907c41239a3d564b35d1b21db65635488b8b92
Ben Anderson <andersjbe@gmail.com>
2020-07-19 19:49:38 -0600

    Fixed error

---
+++ routes/comments.js
@@ -0,0 +1,32 @@
+const { Comment } = require("../db/models");
+const { asyncHandler } = require("../utils");
+const { requireAuth } = require("../auth");
+
+const express = require("express");
+
+const router = express.Router();
+
+router.post(
+  "/:songId(\\d+)",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const songId = req.params.songId;
+    const userId = req.user.id;
+    const { contentBody } = req.body;
+    await Comment.create({ songId, userId, contentBody });
+    res.status(200);
+  })
+);
+
+//  Triggered by a delete button that only renders if the current user from req.user is the same as the userId of the comment
+router.delete(
+  "/:id(\\d+)",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const comment = await Comment.findByPk(req.params.id);
+    await Comment.destroy();
+    res.status(200);
+  })
+);
+
+module.exports = router;
